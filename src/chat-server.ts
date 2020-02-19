31d9dc61b2b8ed256fef0bc0bf99eefd0b1c109d
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-02-19 12:17:23 +0100

    Update to ws instead of SocketIO

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -1,6 +1,6 @@
 import {createServer, Server } from 'http';
 import express from 'express';
-import socketIO from 'socket.io';
+import * as WebSocket from 'ws';
 
 import { MessageModel } from './models/message-model';
 
@@ -8,7 +8,7 @@ export class ChatServer {
     public static readonly PORT:number = 9090;
     private app: express.Application;
     private server: Server;
-    private io: socketIO.Server;
+    private io: any;
     private port: string | number;
 
     public constructor() {
@@ -32,7 +32,8 @@ export class ChatServer {
     }
 
     private sockets(): void {
-        this.io = socketIO(this.server);
+        //const server = createServer(this.app);
+        this.io = new WebSocket.Server({ server: this.server });
     }
 
     private listen(): void {
@@ -40,11 +41,23 @@ export class ChatServer {
             console.log('Running server on port %s', this.port);
         });
 
-        this.io.on('connect', (socket: any) => {
+        this.io.on('connection', (socket: any) => {
             console.log('Connected client on port %s.', this.port);
+            
+            // Try to broadcast a new guest connection message
+            let clientNumber: number = 1;
+            this.io.clients.forEach((client: any) => {
+                console.log(`Broadcast to client ${clientNumber}`);
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify('Hi all, new client [' + clientNumber + '] was connected'));
+                }
+                clientNumber++;
+            });
+
             socket.on('message', (m: MessageModel) => {
                 console.log('[server](message): %s', JSON.stringify(m));
-                this.io.emit('message', m);
+                const response: string = `Says pong to ${JSON.stringify(m)}`;
+                socket.send(JSON.stringify(response));
             });
 
             socket.on('disconnect', () => {
